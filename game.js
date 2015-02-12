b579cd0f7512fc9bf3cf5fec2254f398912b1c0d
Netta Rutenberg <nettaru@gmail.com>
2015-02-12 09:52:07 +0100

    added some logic to frame, bind frame to introEnd

--- game.js
+++ game.js
@@ -5,7 +5,7 @@
 			var score = 0;
 
 			return {
-				roll: function(){ return Math.floor(Math.random()*10) + 1; },
+				roll: function(){ return Math.floor(Math.random()*11); },
 
 				getScore: function() { return score; },
 
@@ -13,21 +13,60 @@
 			};
 		})();
 
-		var frame = (function(frameNum) {
-			var number = frameNum;
+		var frame = (function() {
+			var number = 0;
+			var roll = 1;
+			var rollMax = 2;
 			var $el = jquery('#frame');
+			var startTurn = function(){
+				$el.addClass('animate');
+
+			};
+			var bindEvents = function(){
+				$el.on('start', startTurn);
+				$el.find('button').on('click', function(){
+					var score = player.roll();
+					if (score = 10) {
+						rollMax = (roll === 1) ? 4 : 3;
+					};
+					roll++;
+
+					animate(score, roll);
+				});
+			}();
 
 			return {
 				getNumber: function(){ return number; },
 
-				startTurn: function(){ ; },				
+				startTurn: function(){ },
+
+				getEl:	function(){ return $el; },
 			}
 		})();
 
-		var intro = (function(){
+		var intro = (function($nextEl){
 			var $el = jquery('#intro');
 
-			$el.find('h1').on('webkitAnimationEnd', function(){$el.addClass('animate');});
+			var bindEvents = function(){
+				$el.find('h1').on('webkitAnimationEnd', function(){
+					$el.on('webkitAnimationEnd', function(){
+						$nextEl.trigger('start');
+					});
+					$el.addClass('animate');
+				});
+			};
+			
+			return {
+				animate: function(){
+					bindEvents();
+					$el.children().addClass('animate');
+				}
+			}
+		})(frame.getEl());
+
+		var play = (function(){
+			intro.animate();
+
 		})();
 	})();
 })(jQuery);
