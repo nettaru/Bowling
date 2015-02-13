ddd0717f91f9730b937b2675b42e4a880c2306f0
Netta Rutenberg <nettaru@gmail.com>
2015-02-13 13:03:56 +0100

    clean code a bit, added score keeping, added ball animation

--- game.js
+++ game.js
@@ -3,13 +3,20 @@
 	var game = (function(){
 		var player = (function(){
 			var score = 0;
+			var currentNumOfPins;
+			$ball = jquery('#ball');
+			$ball.on('webkitAnimationEnd', function(){
+				$ball.removeClass('animate');
+				frame.updateCounters(score, currentNumOfPins);
+			});
 
 			return {
-				roll: function(){ return Math.floor(Math.random()*11); },
-
-				getScore: function() { return score; },
-
-				setScore: function(numOfPins) { score += numOfPins; }
+				roll: function(){ 
+					currentNumOfPins = Math.floor(Math.random()*11); 
+					$ball.addClass('animate');
+					score += currentNumOfPins;
+					return currentNumOfPins;
+				}
 			};
 		})();
 
@@ -18,55 +25,55 @@
 			var roll = 1;
 			var rollMax = 2;
 			var $el = jquery('#frame');
-			var startTurn = function(){
-				$el.addClass('animate');
+			var $turn = $el.find('#turn label');
+			var $roll = $el.find('#roll');
+			var $counters = $el.find('#counters');
+			var $score = $el.find('#score .value');
 
-			};
-			var bindEvents = function(){
-				$el.on('start', startTurn);
-				$el.find('button').on('click', function(){
-					var score = player.roll();
-					if (score = 10) {
-						rollMax = (roll === 1) ? 4 : 3;
-					};
-					roll++;
-
-					animate(score, roll);
-				});
-			}();
+			$el.find('button').on('click', function(){
+				var numOfPins = player.roll();
+				if (numOfPins == 10) {
+					if (roll === 1) {
+						rollMax = 4;
+						counters.addClass('strike');
+					} else if (roll === 2) {
+						rollMax = 3;
+						counters.addClass('spare');
+					}
+				}
+			});
 
 			return {
-				getNumber: function(){ return number; },
+				startTurn: function(){
+					$el.addClass('animate');
+				},
 
-				startTurn: function(){ },
-
-				getEl:	function(){ return $el; },
+				updateCounters: function(score, numOfPins) {
+					$roll.find(".roll" + roll + " .value").text(numOfPins);
+					$score.text(score);
+					roll++;
+				}
 			}
 		})();
 
 		var intro = (function($nextEl){
 			var $el = jquery('#intro');
 
-			var bindEvents = function(){
-				$el.find('h1').on('webkitAnimationEnd', function(){
-					$el.on('webkitAnimationEnd', function(){
-						$nextEl.trigger('start');
-					});
-					$el.addClass('animate');
+			$el.find('h1').on('webkitAnimationEnd', function(){
+				$el.on('webkitAnimationEnd', function(){
+					frame.startTurn();
 				});
-			};
+				$el.addClass('animate');
+			});
 			
 			return {
 				animate: function(){
-					bindEvents();
 					$el.children().addClass('animate');
 				}
 			}
-		})(frame.getEl());
-
-		var play = (function(){
-			intro.animate();
-
 		})();
+
+		intro.animate();
+
 	})();
 })(jQuery);
